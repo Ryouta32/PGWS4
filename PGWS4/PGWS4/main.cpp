95a2019bd49acea8e110c6592635245c28ecfb30
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2023-11-01 18:10:07 +0900

    ディスクリプタまで

---
+++ PGWS4/PGWS4/main.cpp
@@ -0,0 +1,215 @@
+﻿#include<windows.h>
+#include<tchar.h>
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<vector>
+#ifdef _DEBUG
+#include<iostream>
+#endif
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+using namespace std;
+
+//@brief コンソール画面にフォーマット付き文字列を表示
+//@param formatフォーマット
+
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist,format);
+	printf(format, valist);
+	va_end(valist);
+#endif // DEBUG
+}
+
+#ifdef _DEBUG
+//面倒だけど書かなきゃいけない
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//ウィンドウが破棄されたら呼ばれる
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);//OSに対して「このアプリは終わる」と伝える
+		return 0;
+}
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+int main() {
+
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#endif
+	//DebugOutputFormatString("Show window test.");
+	//getchar();
+	
+	const unsigned int window_width = 1280;
+	const unsigned int window_height = 720;
+
+	ID3D12Device* _dev = nullptr;
+	IDXGIFactory6* _dxgiFactory = nullptr;
+	IDXGISwapChain4* _swapchain = nullptr;
+	ID3D12CommandAllocator* _cmdAllocator = nullptr;
+	ID3D12CommandQueue* _cmdQueue = nullptr;
+	ID3D12GraphicsCommandList* _cmdList = nullptr;
+
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;//コールバック関数
+	w.lpszClassName = _T("DX12Sample");
+	w.hInstance = GetModuleHandle(nullptr);
+
+	RegisterClassEx(&w);
+
+	RECT wrc = { 0,0,window_width,window_height };
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(w.lpszClassName,//クラス名
+		_T("DX12テスト"),//タイトルバー
+		WS_OVERLAPPEDWINDOW,//タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT,//表示ｘ座標。ＯＳに任せてる
+		CW_USEDEFAULT,//表示Ｙ座標
+		wrc.right - wrc.left,//ウィンドウ幅
+		wrc.bottom - wrc.top,//ウィンドウ高
+		nullptr,//親ウィンドウハンドル
+		nullptr,//メニューバンドル
+		w.hInstance,//呼び出しアプリケーションバンドル
+		nullptr);
+
+	HRESULT D3D12CreateDevice(
+		IUnknown * pAdapter,//自動でグラフィックドライバを選択
+		D3D_FEATURE_LEVEL MinimumFeatureLevel,//最低限必要なフィーチャーレベル
+		REFIID riid,//受け取りたいオブジェクトの肩を識別するためのID
+		void** ppDevice
+	);
+
+	HRESULT CreateSwapChainForHwnd(
+		IUnknown * pDevice,//コマンドキューオブジェクト
+		HWND hWnd,//ウィンドウハンドル
+		const DXGI_SWAP_CHAIN_DESC1 * pDesc,//スワップチェーン設定
+		const DXGI_SWAP_CHAIN_FULLSCREEN_DESC * pFullscreenDesc,//とりあえずnullでok
+		IDXGIOutput * pRestricToOutput,//これもnull
+		IDXGISwapChain1 * *ppSwapChain
+	);
+
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+	auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+	
+	//アダプターの列挙用
+	std::vector<IDXGIAdapter*>adapters;
+	IDXGIAdapter* tmpAdapter = nullptr;
+	for (int i = 0;
+		_dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND;
+		++i) {
+		adapters.push_back(tmpAdapter);
+	}
+	for (auto adpt : adapters) {
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc);//アダプターの説明オブジェクト所得
+		std::wstring strDesc = adesc.Description;
+		//探したいアダプタの名前を確認
+		if (strDesc.find(L"NVIDIA") != std::string::npos) {
+			tmpAdapter = adpt;
+			break;
+		}
+	}
+	//Direct3D デバイスの初期化
+	D3D_FEATURE_LEVEL featureLevel;
+	for (auto lv : levels) {
+		if (D3D12CreateDevice(tmpAdapter, lv, IID_PPV_ARGS(&_dev)) == S_OK) {
+			featureLevel = lv;
+			break;
+		}
+	}
+
+	//コマンドリストの生成
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(&_cmdAllocator));
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+		_cmdAllocator, nullptr,
+		IID_PPV_ARGS(&_cmdList));
+
+	//コマンドキューの作製
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	//タイムアウト梨
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	//アダプタを１つしか使わないときは０でいい
+	cmdQueueDesc.NodeMask = 0;
+	//プライオリティは特になし
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	//コマンドリストと合わせる
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	//キュー作成
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+
+	DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
+	swapchainDesc.Width = window_width;
+	swapchainDesc.Height = window_height;
+	swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapchainDesc.Stereo = false;
+	swapchainDesc.SampleDesc.Count = 1;
+	swapchainDesc.SampleDesc.Quality = 0;
+	swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapchainDesc.BufferCount = 2;
+	//バックバッファーは伸び縮み可能
+	swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
+	//フリップ後は破棄
+	swapchainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	//指定なし
+	swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	//ウィンドウ、フルスク切り替え可能
+	swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	
+	result = _dxgiFactory->CreateSwapChainForHwnd(
+		_cmdQueue, hwnd, &swapchainDesc, nullptr, nullptr, (IDXGISwapChain1**)&_swapchain);
+		
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+	heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	heapDesc.NodeMask = 0;
+	heapDesc.NumDescriptors = 2;//裏表の２つ
+	heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;//特になし
+
+	ID3D12DescriptorHeap* rtvHeaps = nullptr;
+	result = _dev->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&rtvHeaps));
+
+	DXGI_SWAP_CHAIN_DESC swcDesc = {};
+	result = _swapchain->GetDesc(&swcDesc);
+
+	std::vector<ID3D12Resource*> _backBuffers(swcDesc.BufferCount);
+	for (int idx = 0; idx < swcDesc.BufferCount; idx++) {
+		result = _swapchain->GetBuffer(idx, IID_PPV_ARGS(&_backBuffers[idx]));
+		D3D12_CPU_DESCRIPTOR_HANDLE handle
+			= rtvHeaps->GetCPUDescriptorHandleForHeapStart();
+		handle.ptr += idx * _dev->GetDescriptorHandleIncrementSize(
+			D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+		_dev->CreateRenderTargetView(_backBuffers[idx], nullptr, handle);
+	}
+
+	ShowWindow(hwnd, SW_SHOW);
+
+
+	while (true)
+	{
+		MSG msg;
+		if(PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)){
+
+			//アプリが終わるときにmessageがWM＿QUITになる
+			if (msg.message == WM_QUIT) {
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+	return 0;
+}
+
